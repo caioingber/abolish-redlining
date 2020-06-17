ef10ec11b45c5a38e3d51cf3623e7d629d368a44
caio_ingber <caioingber@gmail.com>
2020-06-17 15:15:58 -0400

    built navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav>
+    <div class="nav-container">
+      <a href="#">About Redlining</a>
+      <a href="#">Reinvestment</a>
+      <a href="#">Organizations</a>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/global.scss";
+
+nav {
+  background-color: $nav-color;
+  height: 80px;
+  @include flex(center, center, row);
+  .nav-container {
+    @include flex(space-between, center, row);
+    width: 80%;
+  }
+}
+</style>
